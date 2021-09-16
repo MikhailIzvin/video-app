1086afe0661cbffb674741fd0f3ba636b9c1a2ba
Mikhail Izvin <mikhailizvin@MBP-Mikhail.Dlink>
2021-09-16 13:52:15 +0500

    initial OpenGL and GLFW

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <cstdlib>
+#include <GLFW/glfw3.h>
+
+int main(){
+    GLFWwindow* window;
+    if(!glfwInit()){
+        std::printf("Couldn't init GLFW\n");
+        return 1;
+    }
+
+    window = glfwCreateWindow(640, 480, "myWindow", NULL, NULL);
+    if(!window){
+        std::printf("Couldn't open window\n");
+        return 1;
+    }
+
+    uint8_t* data = new uint8_t[100*100*3];
+    for (int y = 0; y < 100; ++y){
+        for (int x = 0; x < 100; ++x){
+            data[y * 100 * 3 + x * 3    ] = 0xff;
+            data[y * 100 * 3 + x * 3 + 1] = 0x00;
+            data[y * 100 * 3 + x * 3 + 2] = 0x00;
+        }
+    }
+
+    glfwMakeContextCurrent(window);
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+    return 0;
+}
